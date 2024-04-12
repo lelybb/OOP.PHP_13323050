06736f8d369f684fcbe37aaccfcfc102445a6195
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 20:01:43 +0700

    Create CetakInfoProduk.php

---
+++ namespace/App/Produk/CetakInfoProduk.php
@@ -0,0 +1,19 @@
+<?php 
+class CetakInfoProduk {
+    public $daftarProduk = array();
+
+    public function tambahProduk(Produk $produk){
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak(){
+        $str = "DAFTAR PRODUK : <br> <hr>";
+        foreach ($this->daftarProduk as $produk){
+            $str .= "-{$produk->getInfoProduk()} <br>";
+        }
+        return $str;
+    }
+}
+
+
+?>
