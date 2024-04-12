bbdab8c47eff9ea41c85a6f79bf71d8a903e4659
lelybb <lelybutarbutar15@gmail.com>
2024-04-12 20:01:37 +0700

    Create index.php

---
+++ namespace/App/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'init.php';
+
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 25000, 50);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+
+// echo "<hr>";
+
+// new Coba();
+
+use App\Service\User;
+
+
+
+new User();
+echo "<br>";
+new App\Produk\User();
+
+//new App\Produk\User();
+// echo "<br>";
+// new App\Service\User();
+?>
